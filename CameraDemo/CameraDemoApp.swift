f44818b4fb2d74965774a8cf30082a55994a59a3
PPG <g277321@gmail.com>
2022-08-28 10:43:22 +0200

    Initial Commit

---
+++ CameraDemo/CameraDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraDemoApp.swift
+//  CameraDemo
+//
+//  Created by PPG on 28.08.22.
+//
+
+import SwiftUI
+
+@main
+struct CameraDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
